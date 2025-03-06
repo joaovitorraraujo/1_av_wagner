e6c61e2bbe055dff60d8c500014206fe9dbe0fca
Joao <joaovitor19.03.2004@gmail.com>
2025-03-05 23:35:23 -0300

    modifild

--- contas/models.py
+++ contas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Categoria(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
@@ -13,15 +13,17 @@ class Categoria(models.Model):
     def __str__(self):
         return self.name
     
-class Transacao(models.Model):
-    
-
+class Transaction(models.Model):
+    date = models.DateTimeField()
+    description = models.CharField(max_length=200)
+    value = models.DecimalField(max_digits=7, decimal_places=2)
+    observations = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
    
-
     class Meta:
 
         verbose_name = 'Transacao'
         verbose_name_plural = 'Transacaos'
 
     def __str__(self):
-        pass
+        return self.description
